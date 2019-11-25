23bcdd745764c22c665e278f3c5a008da29f4462
yasir khan <ykhan5439@gmail.com>
2019-11-25 20:01:37 +0530

    logs e

--- app/src/main/java/com/example/gitforandroid/MainActivity.java
+++ app/src/main/java/com/example/gitforandroid/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Started");
+        Log.d(TAG, "onCreate: Started");
     }
 }
